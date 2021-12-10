c4e6477291c2e76cabb43681315349cb238e6adc
snowwhitedev <snowwhite91924@gmail.com>
2021-12-10 09:44:08 +0700

    feat: added super owner

--- contracts/TribeOne.sol
+++ contracts/TribeOne.sol
@@ -3,13 +3,13 @@ pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "./interfaces/ITribeOne.sol";
 import "./interfaces/IAssetManager.sol";
+import "./libraries/Ownable.sol";
 import "./libraries/TribeOneHelper.sol";
 
 contract TribeOne is ERC721Holder, ERC1155Holder, ITribeOne, Ownable, ReentrancyGuard {
